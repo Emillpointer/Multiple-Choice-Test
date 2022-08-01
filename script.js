a815ed6e75320bd63540d816ef6cfadea6b0cf8b
Eric Millpointer <emillpointer123@gmail.com>
2022-08-01 12:47:09 -0500

    Second commit

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ var quizScore = 0;
 
 var questions = [
     {
-        question: 'which one of these is a Javascript framework?',
-        answers :[
+        question : 'Which one of these is a Javascript framework?',
+        choices :[
             { text: 'Python', correct: false},
             { text: 'Django', correct: false},
             { text: 'React', correct: true},
@@ -21,3 +21,27 @@ var questions = [
         ]
     }
 ]
+
+function displayQuestions(){
+    var h1 = document.createElement('h1')
+    h1.textContent = questions[0].question;
+
+    questionsArea.append('h1');
+    
+    questions[0].choices.forEach(choice)
+}
+
+var timeLeft = document.getElementById('timer-sec')
+
+var time = 60;
+
+function startTime() {
+    time--
+    timeLeft.textContent = time;
+}
+
+function subtractTime() {
+    setInterval(startTime, 1000)
+}; 
+// This calls the function when start button is clicked
+startButton.addEventListener('click', subtractTime)
